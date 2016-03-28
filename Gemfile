4cd5d8f2b27e7ff0b427317a1c09667a164f77dc
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2016-03-27 22:50:28 -0600

    Initiated the capistrano

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+group :development do
+  gem 'capistrano-rails', '~> 1.1'
+end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "filmoteca"
+set :repo_url,  "git@github.com:filmoteca/filmoteca.git"
+set :scm, :git
+set :keep_releases, 3
+set :deploy_to, "/vagrant/#{fetch(:application)}"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# Example of invocation:
+# cap production deploy BRANCH=2.0.1
+#
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
